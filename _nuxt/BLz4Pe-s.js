9e726f082b87b45a027481f2c62648c2dd364cf1
mardu <mart.noumees@agileworks.eu>
2024-07-01 23:02:40 +0300

    nuxt test

---
+++ _nuxt/BLz4Pe-s.js
@@ -0,0 +1,1 @@
+import{m as a,S as e,T as n,G as p,H as r}from"./KaTgO7Az.js";const c=a({name:"HtmlParagraph",props:{...e,tag:{type:String,default:"p"}},setup(t,o){const s=n(t);return()=>p(t.tag,r(s),o.slots)}});export{c as _};
