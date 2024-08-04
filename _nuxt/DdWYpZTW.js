ce875bd85423be403bc7b4643e2e0dd152399215
mardu <mart.noumees@agileworks.eu>
2024-08-04 14:14:14 +0300

    second push

---
+++ _nuxt/DdWYpZTW.js
@@ -0,0 +1,1 @@
+import{q as a,s as r,u as e}from"./DdW8waKO.js";import{B as n,u as p}from"./BpnoJP4Q.js";const c=a({name:"HtmlParagraph",props:{...n,tag:{type:String,default:"p"}},setup(o,t){const s=p(o);return()=>r(o.tag,e(s),t.slots)}});export{c as _};
